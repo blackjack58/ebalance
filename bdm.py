c5fda281381bf9af39ae3238ac0d2b91fabc76f3
blackjack58 <107323929+blackjack58@users.noreply.github.com>
2023-06-02 08:58:06 -0500

    Update bdm.py

--- bdm.py
+++ bdm.py
@@ -672,7 +672,7 @@ class oil():
                    fancybox=True, framealpha=0.7, 
                    handlelength=0, handletextpad=0) 
 
-        plt.tight_layout(1)                 
+        plt.tight_layout(0)                 
 
         plt.show()       
 
